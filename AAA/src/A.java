49012a4d97b3f8422dd656f62a5ab7df550978c3
User <User@192.168.1.14>
2017-09-22 11:52:41 +0530

    Initial checking of A

---
+++ AAA/src/A.java
@@ -0,0 +1,17 @@
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A aa=new A();
+		aa.display();
+		BB b=new BB();
+		b.bb();
+
+	}
+	public void display()
+	{
+		
+		System.out.println("This is display() in AA Class");
+	}
+}
---
+++ AAA/src/BB.java
@@ -0,0 +1,8 @@
+
+public class BB {
+	public void bb()
+	{
+		
+		System.out.println("This is bb() in BB Class");
+	}
+}
